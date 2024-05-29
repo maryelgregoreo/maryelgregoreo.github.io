11fbb636919df574b666bc24e3b7d59525c7779f
maryelgregoreo <maryelgregorio55@gmail.com>
2024-05-30 01:35:21 +0400

    home_page

---
+++ home.js
@@ -0,0 +1,24 @@
+// pokemon arrow clicker
+
+const slides = document.querySelectorAll('.slide');
+let currentSlide = 0;
+
+function showSlide(index) {
+    slides.forEach((slide, i) => {
+        if (i === index) {
+            slide.classList.add('active');
+        } else {
+            slide.classList.remove('active');
+        }
+    });
+}
+
+function nextSlide() {
+    currentSlide = (currentSlide + 1) % slides.length;
+    showSlide(currentSlide);
+}
+
+function prevSlide() {
+    currentSlide = (currentSlide - 1 + slides.length) % slides.length;
+    showSlide(currentSlide);
+}
